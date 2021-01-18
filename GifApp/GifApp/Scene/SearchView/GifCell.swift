e4eb471fb10d2b591c0ec5492dd9af40f6ca544b
1Consumption <hanseop95@gmail.com>
2021-01-18 16:52:54 +0900

    💄 GifCell 생성
    #3

---
+++ GifApp/GifApp/Scene/SearchView/GifCell.swift
@@ -0,0 +1,18 @@
+//
+//  GifCell.swift
+//  GifApp
+//
+//  Created by 신한섭 on 2021/01/18.
+//
+
+import UIKit
+
+final class GifCell: UICollectionViewCell {
+
+    @IBOutlet weak var favoriteButton: UIButton!
+    @IBOutlet weak var gifImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}

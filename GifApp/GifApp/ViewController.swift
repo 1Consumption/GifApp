ac22e7c095be24fb3155cacac5ab258b40a57e7c
1Consumption <hanseop95@gmail.com>
2021-01-18 15:50:02 +0900

    💄 UINaviagtionController embed
    - barStyle = .black
    - 배경을 투명 처리(view의 배경이 보이도록)
    - title은 Search로 설정
    - tintColor는 white로 설정
    
    close #1

--- GifApp/GifApp/ViewController.swift
+++ GifApp/GifApp/ViewController.swift
@@ -11,5 +11,14 @@ final class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpNavigationBar()
+    }
+    
+    private func setUpNavigationBar() {
+        navigationController?.navigationBar.barStyle = .black
+        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        navigationController?.navigationBar.shadowImage = UIImage()
+        navigationController?.navigationBar.tintColor = .white
+        navigationItem.title = "Search"
     }
 }

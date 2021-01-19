aa269cfefcdb3ddb54ddc42045454825186ebb7c
1Consumption <hanseop95@gmail.com>
2021-01-20 02:20:50 +0900

    ✨ GifCellViewModel gifDelivered output 바인딩
    close #12

--- GifApp/GifAppTests/Model/ViewModel/GifCellViewModelTests.swift
+++ GifApp/GifAppTests/Model/ViewModel/GifCellViewModelTests.swift
@@ -15,9 +15,9 @@ final class GifCellViewModelTests: XCTestCase {
     
     func testViewModelOutputGifInfoDelivered() {
         let expectation = XCTestExpectation(description: "gif delivered")
-        defer { wait(for: [expectation], timeout: 1.0) }
+        defer { wait(for: [expectation], timeout: 5.0) }
         
-        let viewModel = GifCellViewModel()
+        let viewModel = GifCellViewModel(gifURL: "https://media1.giphy.com/media/JuB8b7G4oGsCNALT8I/giphy.gif?cid=cde2429b879azk7f5wj6dy8nqe6c05x0p655p2ayri1hhs6e&rid=giphy.gif")
         
         let output = viewModel.transform(input).gifDelivered
         

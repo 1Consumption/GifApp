33b6d5355e5c8f7dcb1b01e88d6afc63b2fba9e9
1Consumption <hanseop95@gmail.com>
2021-01-20 01:00:52 +0900

    ✨ TrendingViewModel gifInfo delivered output 바인딩
    #11

---
+++ GifApp/GifApp/Model/ViewModel/TrendingGifViewModel.swift
@@ -0,0 +1,40 @@
+//
+//  TrendingGifViewModel.swift
+//  GifApp
+//
+//  Created by 신한섭 on 2021/01/20.
+//
+
+import Foundation
+
+struct TrendingGifViewModelInput {
+    let loadGifInfo: Observable<Void> = Observable<Void>(value: ())
+}
+
+struct TrendingGifViewModelOtuput {
+    let gifInfoDelivered: Observable<[GifInfo]> = Observable<[GifInfo]>(value: [])
+    let errorDelivered: Observable<UseCaseError?> = Observable<UseCaseError?>(value: nil)
+}
+
+final class TrendingGifViewModel: ViewModelType {
+    
+    private let useCase: TrendingGifUseCaseType
+    private var bag: CancellableBag = CancellableBag()
+    
+    init(useCase: TrendingGifUseCaseType = TrendingGifUseCase()) {
+        self.useCase = useCase
+    }
+    
+    func transform(_ input: TrendingGifViewModelInput) -> TrendingGifViewModelOtuput {
+        let output = TrendingGifViewModelOtuput()
+        
+        input.loadGifInfo.bind { [weak self] in
+            self?.useCase.retrieveGifInfo(failureHandler: { _ in},
+                                          successHandler: { gifInfoResponse in
+                                            output.gifInfoDelivered.value = gifInfoResponse.data
+                                          })
+        }.store(in: &bag)
+        
+        return output
+    }
+}

4abbfaf390405b9f32f54c9f9d6804793ecd1e59
1Consumption <hanseop95@gmail.com>
2021-01-19 20:16:06 +0900

    ✨ UseCaseError 추가
    #9

---
+++ GifApp/GifApp/Model/UseCaseError.swift
@@ -0,0 +1,22 @@
+//
+//  UseCaseError.swift
+//  GifApp
+//
+//  Created by 신한섭 on 2021/01/19.
+//
+
+import Foundation
+
+enum UseCaseError: Error {
+    case networkError(with: NetworkError)
+    case decodeError
+    
+    var message: String {
+        switch self {
+        case .networkError(let error):
+            return error.message
+        case .decodeError:
+            return "decode error"
+        }
+    }
+}

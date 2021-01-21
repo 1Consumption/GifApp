6a39e5000f6a01fa158bfdd3bdab84f3494ec638
1Consumption <hanseop95@gmail.com>
2021-01-22 02:32:44 +0900

    ✅ AutoCompleteUseCase 테스트 코드 작성
    #24

---
+++ GifApp/GifAppTests/SearchView/SearchView/AutoCompleteUseCaseTests.swift
@@ -0,0 +1,71 @@
+//
+//  AutoCompleteUseCaseTests.swift
+//  GifAppTests
+//
+//  Created by 신한섭 on 2021/01/22.
+//
+
+@testable import GifApp
+import XCTest
+
+final class AutoCompleteUseCaseTests: XCTestCase {
+
+    func testRetrieveAutoCompleteSuccess() {
+        let expectation = XCTestExpectation(description: "retrieve gifInfo success")
+        defer { wait(for: [expectation], timeout: 1.0) }
+        
+        let model = AutoCompleteResponse(data: [AutoComplete(name: "test")])
+        let data = try! JSONEncoder().encode(model)
+        let networkManager = MockSuccessNetworkManager(data: data)
+        let useCase = AutoCompleteUseCase(networkManager: networkManager)
+        
+        useCase.retrieveAutoComplete(failureHandler: { _ in
+            XCTFail()
+        }, successHandler: {
+            XCTAssertEqual(model, $0)
+            expectation.fulfill()
+        })
+        
+        networkManager.verify(url: EndPoint(urlInfomation: .autoComplete).url,
+                              method: .get,
+                              headers: nil)
+    }
+    
+    func testRetrieveAutoCompleteFailureWithDecodeError() {
+        let expectation = XCTestExpectation(description: "failure with decode error")
+        defer { wait(for: [expectation], timeout: 1.0) }
+        
+        let networkManager = MockFailureNetworkManagerWithDecodeError()
+        let useCase = AutoCompleteUseCase(networkManager: networkManager)
+        
+        useCase.retrieveAutoComplete(failureHandler: { error in
+            XCTAssertEqual(error, .decodeError)
+            expectation.fulfill()
+        }, successHandler: { _ in
+            XCTFail()
+        })
+        
+        networkManager.verify(url: EndPoint(urlInfomation: .autoComplete).url,
+                              method: .get,
+                              headers: nil)
+    }
+    
+    func testRetrieveAutoCompleteFailureWithNetworkError() {
+        let expectation = XCTestExpectation(description: "failure with network error")
+        defer { wait(for: [expectation], timeout: 1.0) }
+        
+        let networkManager = MockFailureNetworkManagerWithNetworkError()
+        let useCase = AutoCompleteUseCase(networkManager: networkManager)
+        
+        useCase.retrieveAutoComplete(failureHandler: { error in
+            XCTAssertEqual(error, .networkError(with: .emptyData))
+            expectation.fulfill()
+        }, successHandler: { _ in
+            XCTFail()
+        })
+        
+        networkManager.verify(url: EndPoint(urlInfomation: .autoComplete).url,
+                              method: .get,
+                              headers: nil)
+    }
+}

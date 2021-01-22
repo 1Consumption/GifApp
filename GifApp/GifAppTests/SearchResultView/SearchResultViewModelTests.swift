a8897c8fd6c5640aa58bc5d86dfb3ac1c65a9496
1Consumption <hanseop95@gmail.com>
2021-01-22 19:38:14 +0900

    ✅ SearchResultViewModel 테스트 케이스 작성
    #28

---
+++ GifApp/GifAppTests/SearchResultView/SearchResultViewModelTests.swift
@@ -0,0 +1,66 @@
+//
+//  SearchResultViewModelTests.swift
+//  GifAppTests
+//
+//  Created by 신한섭 on 2021/01/22.
+//
+
+@testable import GifApp
+import XCTest
+
+final class SearchResultViewModelTests: XCTestCase {
+    
+    private let input: SearchResultViewModelInput = SearchResultViewModelInput()
+    private var bag: CancellableBag = CancellableBag()
+    private var viewModel: SearchResultViewModel!
+    
+    func testNextPageDelivered() {
+        let expactation = XCTestExpectation(description: "next page delivered")
+        defer { wait(for: [expactation], timeout: 1.0) }
+        
+        let useCase = MockSuccessSearchResultUseCase()
+        viewModel = SearchResultViewModel(useCase: useCase)
+        
+        let output = viewModel.transform(input).nextPageDelivered
+        output.bind {
+            useCase.verify(keyword: "test")
+            XCTAssertEqual($0, [IndexPath(row: 0, section: 0)])
+            expactation.fulfill()
+        }.store(in: &bag)
+        
+        input.nextPageRequest.value = "test"
+    }
+    
+    func testErrorDelivered() {
+        let expactation = XCTestExpectation(description: "error delivered")
+        defer { wait(for: [expactation], timeout: 1.0) }
+        
+        let useCase = MockFailureSearchResultUseCase(error: .decodeError)
+        viewModel = SearchResultViewModel(useCase: useCase)
+        
+        let output = viewModel.transform(input).errorDelivered
+        output.bind {
+            useCase.verify(keyword: "test")
+            XCTAssertEqual($0, .decodeError)
+            expactation.fulfill()
+        }.store(in: &bag)
+        
+        input.nextPageRequest.value = "test"
+    }
+    
+    func testShowDetailFired() {
+        let expactation = XCTestExpectation(description: "show detail fired")
+        defer { wait(for: [expactation], timeout: 1.0) }
+        
+        let useCase = DummySearchResultUseCase()
+        viewModel = SearchResultViewModel(useCase: useCase)
+        
+        let output = viewModel.transform(input).showDetailFired
+        output.bind {
+            XCTAssertEqual($0, "test")
+            expactation.fulfill()
+        }.store(in: &bag)
+        
+        input.showDetail.value = "test"
+    }
+}

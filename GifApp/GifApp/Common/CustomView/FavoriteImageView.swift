e403268c58e0a98a2dbfbfbd30864ff32f9119d7
1Consumption <hanseop95@gmail.com>
2021-01-25 01:00:28 +0900

    💄 FavoriteImageView 애니메이션 추가
    - favorite, favoriteCancel 메소드가 호출되면 애니메이션을 실행해 유저에게 Favorite state가 변했음을 알림.
    
    close #40

--- GifApp/GifApp/Common/CustomView/FavoriteImageView.swift
+++ GifApp/GifApp/Common/CustomView/FavoriteImageView.swift
@@ -9,5 +9,53 @@ import UIKit
 
 final class FavoriteImageView: UIImageView {
     
+    private let heart: UIImage? = UIImage(named: "heart")
+    private let heartFill: UIImage? = UIImage(named: "heart.fill")
     
+    func favorite() {
+        image = heartFill
+        transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
+        isHidden = false
+        
+        UIViewPropertyAnimator.runningPropertyAnimator(withDuration: 0.5,
+                                                       delay: 0,
+                                                       options: .allowUserInteraction,
+                                                       animations: { [weak self] in
+                                                        self?.transform = CGAffineTransform(scaleX: 1.5, y: 1.5)
+                                                       },
+                                                       completion: nil)
+        
+        UIViewPropertyAnimator.runningPropertyAnimator(withDuration: 0.2,
+                                                       delay: 0.5,
+                                                       options: .allowUserInteraction,
+                                                       animations: { [weak self] in
+                                                        self?.transform = CGAffineTransform(scaleX: 0.1, y: 0.1)
+                                                       },
+                                                       completion: { [weak self] _ in
+                                                        self?.isHidden = true
+                                                       })
+    }
+    
+    func favoriteCancel() {
+        isHidden = false
+        transform = CGAffineTransform(scaleX: 1.5, y: 1.5)
+        
+        UIViewPropertyAnimator.runningPropertyAnimator(withDuration: 0.5,
+                                                       delay: 0,
+                                                       options: .allowUserInteraction,
+                                                       animations: { [weak self] in
+                                                        self?.image = self?.heart
+                                                       },
+                                                       completion: nil)
+        
+        UIViewPropertyAnimator.runningPropertyAnimator(withDuration: 0.25,
+                                                       delay: 0.5,
+                                                       options: .allowUserInteraction,
+                                                       animations: { [weak self] in
+                                                        self?.transform = CGAffineTransform(scaleX: 0.1, y: 0.1)
+                                                       },
+                                                       completion: { [weak self] _ in
+                                                        self?.isHidden = true
+                                                       })
+    }
 }

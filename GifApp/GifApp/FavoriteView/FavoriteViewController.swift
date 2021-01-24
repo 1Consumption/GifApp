edb49572e473e279fd8cd7b9372188c3a96fb8a6
1Consumption <hanseop95@gmail.com>
2021-01-25 03:11:07 +0900

    💄 FavoriteViewController 생성
    #43

---
+++ GifApp/GifApp/FavoriteView/FavoriteViewController.swift
@@ -0,0 +1,15 @@
+//
+//  FavoriteViewController.swift
+//  GifApp
+//
+//  Created by 신한섭 on 2021/01/25.
+//
+
+import UIKit
+
+final class FavoriteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}

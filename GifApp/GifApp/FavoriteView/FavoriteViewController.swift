7df15cf3a81bb54bf1abd2a75b5c7c6462a1663c
1Consumption <hanseop95@gmail.com>
2021-01-25 03:23:22 +0900

    💄 NavigationController embed
    - bar 색상 투명
    - title은 Favorite
    
    #43

--- GifApp/GifApp/FavoriteView/FavoriteViewController.swift
+++ GifApp/GifApp/FavoriteView/FavoriteViewController.swift
@@ -11,5 +11,17 @@ final class FavoriteViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpNavigationBar()
+    }
+    
+    private func setUpNavigationBar() {
+        navigationItem.backButtonTitle = ""
+        navigationController?.navigationBar.barStyle = .black
+        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        navigationController?.navigationBar.shadowImage = UIImage()
+        navigationController?.navigationBar.tintColor = .white
+        let textAttributes = [NSAttributedString.Key.foregroundColor : UIColor.white]
+        navigationController?.navigationBar.titleTextAttributes = textAttributes
+        navigationItem.title = "Favorite"
     }
 }

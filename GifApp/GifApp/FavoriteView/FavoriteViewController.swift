5715ca4cefe76f26ba86b21517cbef9c85fa8e72
1Consumption <hanseop95@gmail.com>
2021-01-25 16:58:16 +0900

    ✨ FavoriteCollectionVieModel 연동
    - viewWillAppear에서 favorite 정보 요청 이벤트 전달
    
    close #46

--- GifApp/GifApp/FavoriteView/FavoriteViewController.swift
+++ GifApp/GifApp/FavoriteView/FavoriteViewController.swift
@@ -11,6 +11,11 @@ final class FavoriteViewController: UIViewController {
 
     @IBOutlet weak var favoriteCollectionView: UICollectionView!
     
+    private let dataSoruce: GifCollectionViewDataSource = GifCollectionViewDataSource()
+    private let favoriteCollectionViewModel: FavoriteCollectionViewModel = FavoriteCollectionViewModel()
+    private let favoriteCollectionViewModelInput: FavoriteCollectionViewModelInput = FavoriteCollectionViewModelInput()
+    private var bag: CancellableBag = CancellableBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpNavigationBar()
@@ -31,15 +36,40 @@ final class FavoriteViewController: UIViewController {
     private func setUpFavoriteCollectionView() {
         favoriteCollectionView.register(UINib(nibName: GifCell.identifier, bundle: .main), forCellWithReuseIdentifier: GifCell.identifier)
         favoriteCollectionView.delaysContentTouches = false
+        dataSoruce.viewModel = favoriteCollectionViewModel
+        favoriteCollectionView.dataSource = dataSoruce
         if let layout = favoriteCollectionView?.collectionViewLayout as? PinterestLayout {
             layout.delegate = self
         }
+        bindWithFavoriteCollectionViewModel()
+    }
+    
+    private func bindWithFavoriteCollectionViewModel() {
+        let output = favoriteCollectionViewModel.transform(favoriteCollectionViewModelInput)
+        
+        output.favoriteListDelivered.bind {
+            DispatchQueue.main.async { [weak self] in
+                self?.favoriteCollectionView.reloadData()
+            }
+        }.store(in: &bag)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        favoriteCollectionViewModelInput.loadFavoriteList.fire()
     }
 }
 
 extension FavoriteViewController: PinterestLayoutDelegate {
     
     func collectionView(_ collectionView: UICollectionView, heightForPhotoAtIndexPath indexPath: IndexPath) -> CGSize {
-        return .zero
+        guard let dataSource = collectionView.dataSource as? GifCollectionViewDataSource else { return .zero }
+        guard let strWidth = dataSource.gifInfo(of: indexPath.item)?.images.original.width,
+              let strHeight = dataSource.gifInfo(of: indexPath.item)?.images.original.height,
+              let width = Double(strWidth),
+              let height = Double(strHeight)
+        else { return .zero }
+        
+        return CGSize(width: width, height: height)
     }
 }

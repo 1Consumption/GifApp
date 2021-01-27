4e4ea9b20f263e7c210d09bdb82bbce152e0dd62
1Consumption <hanseop95@gmail.com>
2021-01-27 19:03:59 +0900

    🔧 Secret 파일 추가
    - 빌드에 지장이 없도록 부득이하게 API key를 담은 파일 추가

---
+++ GifApp/GifApp/Common/Secret.swift
@@ -0,0 +1,13 @@
+//
+//  Secret.swift
+//  GifApp
+//
+//  Created by 신한섭 on 2021/01/21.
+//
+
+import Foundation
+
+enum Secret {
+    
+    static let APIKey: String = "fvDqP0H7kmN8vKbA6UTZU52bIWA0zMx8"
+}

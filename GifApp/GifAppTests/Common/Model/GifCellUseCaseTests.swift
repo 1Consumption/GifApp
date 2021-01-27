587d583576e0fc0e9992f656a5fe39fc077e5f1b
1Consumption <hanseop95@gmail.com>
2021-01-27 18:03:17 +0900

    ✅ GifCellUseCase 테스트 코드 작성
    - ImageManagerType 프로퍼티를 통해 Gif이미지를 가져올 수 있다.
    - FavoriteType 프로퍼티를 통해 Favorite 이벤트를 전달할 수 있다.

---
+++ GifApp/GifAppTests/Common/Model/GifCellUseCaseTests.swift
@@ -0,0 +1,116 @@
+//
+//  GifCellUseCaseTests.swift
+//  GifAppTests
+//
+//  Created by 신한섭 on 2021/01/27.
+//
+
+@testable import GifApp
+import XCTest
+
+final class GifCellUseCaseTests: XCTestCase {
+
+    private var gifCellUseCase: GifCellUseCase!
+    private let gifInfo: GifInfo = GifInfo(id: "1",
+                                           username: "",
+                                           source: "",
+                                           images: GifImages(original: GifImage(height: "", width: "", url: ""),
+                                                             fixedWidth: GifImage(height: "", width: "", url: "test")),
+                                           user: User(avatarUrl: "", username: "", displayName: ""))
+    
+    func testRetrieveImageSuccess() {
+        let expectation = XCTestExpectation(description: "retrieve image")
+        defer { wait(for: [expectation], timeout: 1.0) }
+        
+        let imageManager = MockSuccessImageManager()
+        let favoriteManager = DummyFavoriteManager()
+        
+        gifCellUseCase = GifCellUseCase(imageManager: imageManager, favoriteManager: favoriteManager)
+        
+        let _ = gifCellUseCase.retrieveImage(with: "test") { result in
+            switch result {
+            case .success:
+                imageManager.verify(url: "test")
+                expectation.fulfill()
+            case .failure:
+                XCTFail()
+            }
+        }
+    }
+    
+    func testRetrieveImageFailure() {
+        let expectation = XCTestExpectation(description: "retrieve image failure")
+        defer { wait(for: [expectation], timeout: 1.0) }
+        
+        let imageManager = MockFailureImageManager()
+        let favoriteManager = DummyFavoriteManager()
+        
+        gifCellUseCase = GifCellUseCase(imageManager: imageManager, favoriteManager: favoriteManager)
+        
+        let _ = gifCellUseCase.retrieveImage(with: "test") { result in
+            switch result {
+            case .success:
+                XCTFail()
+            case .failure:
+                imageManager.verify(url: "test")
+                expectation.fulfill()
+            }
+        }
+    }
+    
+    func testSendFavoriteStateChange() {
+        let expectation = XCTestExpectation(description: "send favorite state change")
+        expectation.expectedFulfillmentCount = 2
+        defer { wait(for: [expectation], timeout: 3.0) }
+        
+        let imageManager = DummyImageManager()
+        let favoriteManager = MockSuccessFavoriteManager()
+        
+        gifCellUseCase = GifCellUseCase(imageManager: imageManager, favoriteManager: favoriteManager)
+        
+        gifCellUseCase.sendFavoriteStateChange(gifInfo: gifInfo) { result in
+            switch result {
+            case .success(let factor):
+                XCTAssertTrue(factor)
+                favoriteManager.verifyChangeFavoriteState(gifInfo: self.gifInfo, storageCount: 1)
+                expectation.fulfill()
+            case .failure:
+                XCTFail()
+            }
+        }
+        
+        DispatchQueue.global().asyncAfter(deadline: .now() + 1, execute: {
+            self.gifCellUseCase.sendFavoriteStateChange(gifInfo: self.gifInfo) { result in
+                switch result {
+                case .success(let factor):
+                    XCTAssertFalse(factor)
+                    favoriteManager.verifyChangeFavoriteState(gifInfo: self.gifInfo, storageCount: 0, callCount: 2)
+                    expectation.fulfill()
+                case .failure:
+                    XCTFail()
+                }
+            }
+        })
+    }
+    
+    func testSendFavoriteStateChangeFailure() {
+        let expectation = XCTestExpectation(description: "send favorite state change failure")
+        defer { wait(for: [expectation], timeout: 1.0) }
+        
+        let imageManager = DummyImageManager()
+        let favoriteManager = MockFailureFavoriteManager(error: .encodeError)
+        
+        gifCellUseCase = GifCellUseCase(imageManager: imageManager, favoriteManager: favoriteManager)
+        
+        gifCellUseCase.sendFavoriteStateChange(gifInfo: gifInfo) { result in
+            switch result {
+            case .success:
+                XCTFail()
+            case .failure(let error):
+                XCTAssertEqual(error, .encodeError)
+                favoriteManager.verifyChangeFavoriteState(gifInfo: self.gifInfo)
+                expectation.fulfill()
+            }
+        }
+    }
+}
